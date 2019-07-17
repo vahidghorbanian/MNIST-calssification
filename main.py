800e52f9e7e2bee6eda04aa4185df2878c83d7b7
vahidghorbanian <47153871+vahidghorbanian@users.noreply.github.com>
2019-07-17 17:50:55 -0400

    commit(3)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from utils import *
 import pickle
-
-
+import matplotlib.pyplot as plt
 
 # %% Read and Load data
 #train_img, train_lbl, train = load_dataset(dataset = "training", path = ".")
@@ -10,12 +9,36 @@ pickle_in = open("data.pickle","rb")
 example_dict = pickle.load(pickle_in)
 train = example_dict['train']
 test = example_dict['test']
+train_img = example_dict['train_img']
+test_img = example_dict['test_img']
+train_lbl = example_dict['train_lbl']
+test_lbl = example_dict['test_lbl']
 
 
 # %% classifications
-result_knn = knn_classifier(train, test)
-result_lr = lr_classifier(train, test)
-result_dt = dt_classifier(train, test)
-result_rf = rf_calssifier(train, test)
-result_Ada = Ada_calssifier(train, test)
-results_svm = SVM_classifier(train, test)
+#result_lr = lr_classifier(train, test)
+#result_knn = knn_classifier(train, test)
+#result_dt = dt_classifier(train, test)
+#result_rf = rf_calssifier(train, test)
+#result_Ada = Ada_calssifier(train, test)
+#result_svm = SVM_classifier(train, test)
+result_nn = nn_classifier(train_img, train_lbl, test_img, test_lbl)
+
+#%% Plot NN results
+print('\nThe results are only plotted for the firsted trained NN model.')
+predict_lbl = np.argmax(result_nn['prediction'][0],axis=1)
+test_lbl = result_nn['test_lbl']
+diff = np.nonzero(test_lbl-predict_lbl)[0]
+row = 7
+col = np.floor(len(diff)/row)
+plt.figure(figsize=(15,8))
+for i in np.arange(0,row*col,1):
+    plt.subplot(row, col, i+1)
+    plt.imshow(test_img[diff[int(i)]])
+    plt.tight_layout
+    plt.xlabel('as:'+str(predict_lbl[diff[int(i)]]))
+    plt.xticks([])
+    plt.yticks([])
+plt.suptitle('Misclassified Samples')
+plt.show()
+    
--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import struct
 import os
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras import regularizers
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.nn import sigmoid, softmax, relu
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 from sklearn.metrics import classification_report
@@ -12,6 +17,7 @@ from sklearn.svm import SVC
 import time
 
 
+#%%
 def load_dataset(dataset = "training", path = "."):
     if dataset is "training":
         fname_img = os.path.join(path, 'train-images.idx3-ubyte')
@@ -35,10 +41,33 @@ def load_dataset(dataset = "training", path = "."):
     return img, lbl, mat
 
 
-num_train = 20000
-num_test = 500
+#%%
+num_train = 60000
+num_test = 1000
 iter = 500
 random_state = 0
+def lr_classifier(train, test):
+    print('\n******************************************')
+    print('Logistic Regression is running...')
+    train = train[0:num_train,:]
+    C = [1]
+    score_test = []
+    model = []
+    for i, c in enumerate(C):
+        start_time = time.time()
+        print('\nC = ', c)
+        model.append(LogisticRegression(C=c, max_iter=iter, solver='lbfgs', multi_class='auto', random_state=random_state))
+        print('Logisitic Regression fit done!')
+        model[i].fit(train[:,:-1], train[:,-1])
+        predict_test = model[i].predict(test[0:num_test,:-1])
+        score_test.append(metrics.accuracy_score(test[0:num_test,-1], predict_test))
+        elapsed_time = time.time() - start_time
+        print('run time: ', elapsed_time)
+    print('Test scores: ', score_test)
+    return {'model': model, 'test score': score_test}
+
+
+#%%
 def knn_classifier(train, test):
     print('\n******************************************')
     print('KNN is running...')
@@ -61,27 +90,7 @@ def knn_classifier(train, test):
     return {'model': model, 'test score': score_test}
 
 
-def lr_classifier(train, test):
-    print('\n******************************************')
-    print('Logistic Regression is running...')
-    train = train[0:num_train,:]
-    C = [1]
-    score_test = []
-    model = []
-    for i, c in enumerate(C):
-        start_time = time.time()
-        print('\nC = ', c)
-        model.append(LogisticRegression(C=c, max_iter=iter, solver='lbfgs', multi_class='multinomial', random_state=random_state))
-        print('Logisitic Regression fit done!')
-        model[i].fit(train[:,:-1], train[:,-1])
-        predict_test = model[i].predict(test[0:num_test,:-1])
-        score_test.append(metrics.accuracy_score(test[0:num_test,-1], predict_test))
-        elapsed_time = time.time() - start_time
-        print('run time: ', elapsed_time)
-    print('Test scores: ', score_test)
-    return {'model': model, 'test score': score_test}
-
-
+#%%
 def dt_classifier(train, test):
     print('\n******************************************')
     print('Decision Tree is running...')
@@ -103,6 +112,7 @@ def dt_classifier(train, test):
     return {'model': model, 'test score': score_test}
 
 
+#%%
 def rf_calssifier(train, test):
     print('\n******************************************')
     print('Random forest is running...')
@@ -124,6 +134,7 @@ def rf_calssifier(train, test):
     return {'model': model, 'test score': score_test}
 
 
+#%%
 def Ada_calssifier(train, test):
     print('\n******************************************')
     print('AdaBoost is running...')
@@ -145,12 +156,12 @@ def Ada_calssifier(train, test):
     return {'model': model, 'test score': score_test}
 
 
+#%%
 def SVM_classifier(train, test):
     print('\n******************************************')
     print('SVM is running...')
     train = train[0:num_train,:]
     kernel = ['linear', 'poly', 'rbf']
-#    kernel = ['rbf']
     C = [1, 10, 100, 1000, 10000]
     score_test = []
     score = []
@@ -173,7 +184,58 @@ def SVM_classifier(train, test):
     return {'model': model, 'test score': score_test}
 
 
-
+#%%
+def nn_classifier(train_img, train_lbl, test_img, test_lbl):
+    print('\n******************************************')
+    print('fully connected neural net is running...')
+#    train = train[0:num_train,:]
+    train_img = train_img[0:num_train,:]
+    test_img = test_img[0:num_test,:]
+    train_lbl = train_lbl[0:num_train]
+    test_lbl = test_lbl[0:num_test]
+    num_h = [400]
+    epochs = 30
+    l1_val = 0.0
+    l2_val = 0.0
+    earlystop = True
+    model = []
+    score = []
+    score_test = []
+    predict = []
+    w = []
+    weight = []
+    reg = regularizers.l1_l2(l1=l1_val, l2=l2_val)
+    model = [tf.keras.Sequential([Flatten(input_shape=(np.shape(train_img)[1], np.shape(train_img)[2])),
+                                  Dense(64, activation=sigmoid, kernel_regularizer=reg),
+                                  Dense(64, activation=sigmoid, kernel_regularizer=reg),
+                                  Dense(10, activation=tf.nn.softmax)])]
+    for j, m in enumerate(model):
+        num_layer = len(m.layers)          
+        for i, n in enumerate(num_h):
+            for k in np.arange(1, num_layer-1, 1):
+                m.get_layer(index=k).units = n
+#            models.append(m)
+            print('\nnum.dense layers= ', num_layer-2,' num.neurons=', n, ' l1=', l1_val, ' l2=', l2_val)
+            m.compile(optimizer='Adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+            if earlystop == False:
+                print('Early Stopping deactivated!')
+                m.fit(train_img, train_lbl, epochs=epochs)
+            else:
+                print('Early Stopping activated!')
+                callbacks = [EarlyStopping(monitor='val_loss', patience=2)]
+                m.fit(train_img, train_lbl, epochs=epochs, callbacks=callbacks,
+                      validation_split =0.2, shuffle=False)
+            score.append(m.evaluate(test_img, test_lbl))
+            predict.append(m.predict(test_img))
+            w.append(m.get_weights())
+        score_test.append(score)
+        weight.append(w)
+    print('models:\n', model)
+    print('number of hidden units:\n', num_h)
+    print('test scores:\n', score_test)
+    return {'model': model, 'num_hidden_units': num_h, 'test_score': score_test,
+            'prediction': predict, 'test_lbl': test_lbl, 'test_img': test_img,
+            'weights': weight}
 
 
 
